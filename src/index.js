57bd04367bddd7cc7385cc994bd8868b39572801
gyanu2507 <81113356+gyanu2507@users.noreply.github.com>
2022-12-26 08:38:07 +0530

    Update index.js
    Firebase keys

--- src/index.js
+++ src/index.js
@@ -3,7 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorker from "./serviceWorker";
+import * as firebase from "firebase";
+import "firebase/firestore";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyCKXD8dwmMzpAe00XTXxNVp_6tYoNG-300",
+  authDomain: "shopping-cart-19c56.firebaseapp.com",
+  projectId: "shopping-cart-19c56",
+  storageBucket: "shopping-cart-19c56.appspot.com",
+  messagingSenderId: "933865059966",
+  appId: "1:933865059966:web:bbb5f194d8faba53fc8a2f",
+  measurementId: "G-4C0JEMYKQY"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -15,3 +29,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+serviceWorker.unregister();
